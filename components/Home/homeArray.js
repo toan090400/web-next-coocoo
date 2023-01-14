47e1609255712a974227be6c50b1a3dc3502f16e
nductoan <toanbeo090400@gmail.com>
2023-01-14 19:08:19 +0700

    item array

---
+++ components/Home/homeArray.js
@@ -0,0 +1,160 @@
+const itemHot = [
+  {
+    id: 1,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+  {
+    id: 2,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+  {
+    id: 3,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+];
+// topList
+import user1 from "../../public/home/topList/user-1.png";
+import user2 from "../../public/home/topList/user-2.png";
+import user3 from "../../public/home/topList/user-3.png";
+import user4 from "../../public/home/topList/user-4.png";
+import user5 from "../../public/home/topList/user-5.png";
+const topList = [
+  {
+    id: 1,
+    name: "CryptoFunks",
+    number: "19,769.39",
+    bnb: "+26.52%",
+    status: true,
+    image: user1,
+    check: true,
+  },
+  {
+    id: 2,
+    name: "Cryptix",
+    number: "9,769.39",
+    bnb: "-10.52%",
+    status: false,
+    image: user2,
+  },
+  {
+    id: 3,
+    name: "Frensware",
+    number: "19,769.39",
+    bnb: "+26.52%",
+    status: true,
+    image: user3,
+  },
+  {
+    id: 4,
+    name: "Punk Art",
+    number: "19,769.39",
+    bnb: "-6.52%",
+    status: false,
+    image: user4,
+  },
+  {
+    id: 5,
+    name: "CryptoFunks",
+    number: "19,769.39",
+    bnb: "+26.52%",
+    status: true,
+    image: user5,
+  },
+];
+// discoverMore
+const discoverMore = [
+  {
+    id: 1,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+  {
+    id: 2,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+  {
+    id: 3,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+  {
+    id: 4,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+  {
+    id: 5,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+  {
+    id: 6,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+  {
+    id: 7,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+  {
+    id: 8,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+  {
+    id: 9,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+  {
+    id: 10,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+  {
+    id: 11,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+  {
+    id: 12,
+    name: "Name of Collectible",
+    discription: "Name of Collectible",
+    bnb: "0.25 BNB",
+    time: "3h 50m 2s",
+  },
+];
+module.exports = { itemHot, topList, discoverMore };
